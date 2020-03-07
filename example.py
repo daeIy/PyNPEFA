c1f8e79316b0da4c52a6f07dd3d34697f8f74b09
Gabriel Daely <61368600+daeIy@users.noreply.github.com>
2020-03-07 12:50:24 +0700

    Create example.py

---
+++ example.py
@@ -0,0 +1,10 @@
+import lasio
+import numpy as np
+from funcs import py_npefa, l1tf, l1tf_lm
+
+# Read data
+y = lasio.read('D:\Erich\Random Project\Well_Data\SEMBAKUNG-5_ARCIND\Log Digital\WCL0001834.LAS').df().GR.dropna()
+x = np.array(y.index.tolist())
+
+# Apply PyNPEFA
+inpefa_log = py_npefa.py_npefa(y,x)
